a1492e6798332e17ea37fb727475b2dcb9322823
xnameoz <33310793+xnameoz@users.noreply.github.com>
2022-07-24 22:42:10 +0700

    Initial Commit
    All files are commited as master commit

---
+++ GeekSeat_AF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekSeat_AF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<WitchSaga.Person> ps = new List<WitchSaga.Person>();
+            WitchSaga.Person p = new WitchSaga.Person(10, 12);
+            ps.Add(p);
+            Console.WriteLine("Person 1 Born On Year = " + p.DeathYear + " - " + p.DeathAge + " = " + p.BornYear.ToString() + ", Number of People Killed on year " + p.BornYear + " is " + p.NumOfPeopleKilled.ToString());
+            p = new WitchSaga.Person(13, 17);
+            ps.Add(p);
+            Console.WriteLine("Person 2 Born On Year = " + p.DeathYear + " - " + p.DeathAge + " = " + p.BornYear.ToString() + ", Number of People Killed on year " + p.BornYear + " is " + p.NumOfPeopleKilled.ToString());
+            float avg = WitchSaga.Person.AverageNumberOfPeopleKilled(ps);
+            Console.WriteLine("So the average is " + avg.ToString());
+            //Console.WriteLine(r.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WitchSaga/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WitchSaga
+{
+    public class Person
+    {
+        public int BornYear { get; set; }
+        public float NumOfPeopleKilled { get; set; }
+        public int DeathAge { get; set; }
+        public int DeathYear { get; set; }
+
+        public Person(int ageOfDeath, int yearOfDeath)
+        {
+            DeathAge = ageOfDeath;
+            DeathYear = yearOfDeath;
+            BornYear = yearOfDeath - ageOfDeath;
+            NumOfPeopleKilled = NumberOfPeopleKilledOnYear(BornYear);
+        }
+
+        public static float AverageNumberOfPeopleKilled(List<Person> Persons)
+        {
+            float vRet = 0;
+            int div = Persons.Count();
+            float totalPeopleKilled = 0;
+            foreach (Person p in Persons)
+            {
+                totalPeopleKilled += p.NumOfPeopleKilled;
+            }
+            vRet = totalPeopleKilled / div;
+            return vRet;
+        }
+
+        int NumberOfPeopleKilledOnYear(int year)
+        {
+            if (year < 0)
+            {
+                return -1;
+            }
+
+            int[] arr = new int[year];
+
+            arr[0] = 1; arr[1] = 1;
+            for (int x = 0; x < year; x++)
+            {
+                if (x == 0)
+                {
+                    arr[0] = 1;
+                }
+                else
+                {
+                    if (x == 1)
+                    {
+                        arr[1] = 1;
+                    }
+                    else
+                    {
+                        arr[x] = arr[x - 1] + arr[x - 2];
+                    }
+                }
+            }
+            int sum = arr.Sum();
+            return sum;
+        }
+    }
+}
